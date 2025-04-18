e36f9afceaa6292adb022ac6a1b4b97cfa202b18
LIJIAN1998 <844299030@qq.com>
2025-04-18 15:12:52 +0800

    fix bug in get dataset info

--- data/utils.py
+++ data/utils.py
@@ -41,7 +41,7 @@ def get_dataset_info(config: Config):
     hrtf_loader = config.hrtf_loader
     if hrtf_loader == "hartufo":
         ds = load_function(data_dir, features_spec=HrirSpec(domain="magnitude", side="left", samplerate=config.hrir_samplerate), subject_ids='first')
-        return ds.fundamental_angles. ds.orthogonal_angles, ds.radii
+        return ds.fundamental_angles, ds.orthogonal_angles, ds.radii
     elif hrtf_loader == "hrtfdata":
         ds = load_function(data_dir, feature_spec={'hrirs': {'samplerate': config.hrir_samplerate,
                                                              'side': 'left', 'domain': config.domain}}, subject_ids='first')
